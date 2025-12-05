d821817a6de3d70b20c2ec0252e5c0815205deda
Diego <diego.lara.rodriguez@alumnos.upm.es>
2025-12-05 21:34:33 +0100

    actualización de ficheros de pruebas

--- src/test/java/org/space_invaders/functional/InitBombTests.java
+++ src/test/java/org/space_invaders/functional/InitBombTests.java
@@ -1,5 +1,6 @@
 package org.space_invaders.functional;
 
+import org.space_invaders.main.Commons;
 import org.space_invaders.space_invaders.sprites.Alien;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -7,10 +8,13 @@ public class InitBombTests {
     @org.junit.jupiter.params.ParameterizedTest
     @org.junit.jupiter.params.provider.CsvFileSource(resources = "/InitBombCases.csv")
     void testInitBomb(int posX, int posY, int expectedX, int expectedY) {
-        Alien alien = new Alien(posX, posY);
+        //TODO modificado la inicialización de alien: Alien alien = new Alien(inputX, inputY);
+        Alien alien = new Alien(0, 0);
         Alien.Bomb bomb = alien.getBomb();
+        //TODO añadido inicialización de bomb
         bomb.initBomb(posX, posY);
-        assertEquals(expectedX, alien.getX(), "La coordenada X no es la esperada.");
-        assertEquals(expectedY, alien.getY(), "La coordenada Y no es la esperada.");
+        //TODO modificados los valores de variables que se comprueban
+        assertEquals(expectedX, alien.getBomb().getX(), "La coordenada X no es la esperada.");
+        assertEquals(expectedY, alien.getBomb().getY(), "La coordenada Y no es la esperada.");
     }
 }
--- src/test/java/org/space_invaders/functional/UpdateAliensTests.java
+++ src/test/java/org/space_invaders/functional/UpdateAliensTests.java
@@ -26,14 +26,18 @@ public class UpdateAliensTests {
     void testUpdateAliensDerrota() {
         System.out.println("\ntestUpdateAliensDerrota()");
         printAliens("situación inicial", board);
+        //TODO si no se indica la dirección la lógica de actualización es inversa
+        board.setDirection(1);
         while (board.getAliens().getLast().getY() <= Commons.GROUND - Commons.ALIEN_HEIGHT){
             board.update_aliens();
         }
         printAliens("situación final", board);
-        if (board.getAliens().getLast().getY() >= Commons.GROUND - Commons.ALIEN_HEIGHT) {
+        System.out.println("estado del juego = "+ board.isInGame());
+        //TODO ahora no hace falta comprobar el caso que sigue a continuación
+        /*if (board.getAliens().getLast().getY() >= Commons.GROUND - Commons.ALIEN_HEIGHT) {
             System.out.println("derrotaY = " + (Commons.GROUND - Commons.ALIEN_HEIGHT) + ", posY = " + board.getAliens().getLast().getY());
             correcto = false;
-        }
+        }*/
         if (board.isInGame()) {
             System.out.println("estado del juego = "+ board.isInGame());
             correcto = false;
@@ -45,22 +49,24 @@ public class UpdateAliensTests {
     void testUpdateAliensBordeDCH() {
         System.out.println("testUpdateAliensBordeDCH()");
         printAliens("situación inicial", board);
-        while (board.getAliens().getLast().getX() <= Commons.BOARD_WIDTH - Commons.BORDER_RIGHT){
-            if (board.getAliens().getLast().getX() == Commons.BOARD_WIDTH - Commons.BORDER_RIGHT) {
-                posY = board.getAliens().getLast().getY();
-                posX = board.getAliens().getLast().getX();
-            }
+        //TODO redefinido hasta línea 57
+        while (board.getAliens().getLast().getX() < Commons.BOARD_WIDTH - Commons.BORDER_RIGHT){
             board.update_aliens();
         }
+        posY = board.getAliens().getLast().getY();
+        posX = board.getAliens().getLast().getX();
+        board.update_aliens();
         printAliens("situación final", board);
-        if (board.getAliens().getLast().getX() >= Commons.BOARD_WIDTH - Commons.BORDER_RIGHT) {
-            System.out.println("borde DCH = " + (Commons.BOARD_WIDTH - Commons.BORDER_RIGHT) + ", posX = " + board.getAliens().getLast().getX());
+        //TODO cambiado >= por >
+        if (board.getAliens().getLast().getX() > Commons.BOARD_WIDTH - Commons.BORDER_RIGHT) {
+            System.out.println("sobrepasado borde DCH = " + (Commons.BOARD_WIDTH - Commons.BORDER_RIGHT) + ", posX = " + board.getAliens().getLast().getX());
             correcto = false;
         }
+        /*TODO no es necesario que se cumpla la condición por la redefinición
         if (board.getAliens().getLast().getX() != posX) {
             System.out.println("board.getAliens().getLast().getX() = " + board.getAliens().getLast().getX() + ", posX = " + posX);
             correcto = false;
-        }
+        }*/
         if (board.getAliens().getLast().getY() != posY + Commons.GO_DOWN) {
             System.out.println("posY + Commons.GO_DOWN = " + (posY + Commons.GO_DOWN) + ", posY = " + board.getAliens().getLast().getY());
             correcto = false;
@@ -75,25 +81,26 @@ public class UpdateAliensTests {
     @Test
     void testUpdateAliensBordeIZQ() {
         System.out.println("testUpdateAliensBordeIZQ()");
-        board.setDirection(1);
-        // @TODO setDirection tiene lógica inversa!
+        //TODO modificado el parámetro de setDirection, de acuerdo a la lógica corregida
+        board.setDirection(-1);
         printAliens("situación inicial", board);
-        while (board.getAliens().getFirst().getX() >= Commons.BORDER_LEFT){
-            if (board.getAliens().getFirst().getX() == Commons.BORDER_LEFT) {
-                posY = board.getAliens().getLast().getY();
-                posX = board.getAliens().getFirst().getX();
-            }
+        //TODO setDirection tenía lógica inversa! & sustituido >= por > & reescrito hasta línea 93
+        while (board.getAliens().getFirst().getX() > Commons.BORDER_LEFT){
             board.update_aliens();
         }
+        posY = board.getAliens().getLast().getY();
+        posX = board.getAliens().getFirst().getX();
+        board.update_aliens();
         printAliens("situación final", board);
         if (Commons.BORDER_LEFT > board.getAliens().getFirst().getX()) {
+            System.out.println("sobrepasado borde IZQ = " + Commons.BORDER_LEFT + ", posX = " + board.getAliens().getFirst().getX());
             correcto = false;
-            System.out.println("borde IZQ = " + Commons.BORDER_LEFT + ", posX = " + board.getAliens().getFirst().getX());
         }
+        /*TODO no es necesario que se cumpla la condición por la redefinición
         if (board.getAliens().getFirst().getX() != posX) {
             System.out.println("board.getAliens().getFirst().getX() = " + board.getAliens().getFirst().getX() + ", anteriorX = " + posX);
             correcto = false;
-        }
+        }*/
         if (board.getAliens().getLast().getY() != posY + Commons.GO_DOWN) {
             correcto = false;
             System.out.println("altura antes del borde = " + posY + ", después = " + board.getAliens().getLast().getY());
@@ -109,6 +116,8 @@ public class UpdateAliensTests {
     void testUpdateAliensNormalDCH() {
         System.out.println("testUpdateAliensNormalDCH()");
         printAliens("situación inicial", board);
+        //TODO si no se indica la dirección la lógica de actualización es inversa
+        board.setDirection(1);
         posX = board.getAliens().getFirst().getX();
         board.update_aliens();
         printAliens("situación final", board);
@@ -122,7 +131,7 @@ public class UpdateAliensTests {
     @Test
     void testUpdateAliensNormalIZQ() {
         System.out.println("testUpdateAliensNormalIZQ()");
-        board.setDirection(1);
+        board.setDirection(-1);
         // @TODO setDirection tiene lógica inversa!
         posX = board.getAliens().getFirst().getX();
         printAliens("situación inicial", board);
