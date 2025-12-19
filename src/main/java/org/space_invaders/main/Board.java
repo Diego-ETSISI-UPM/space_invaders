7a30ab350a9b1e965845a23f4cef603db87d659c
Diego <diego.lara.rodriguez@alumnos.upm.es>
2025-12-19 12:47:23 +0100

    Fixes #6

--- src/main/java/org/space_invaders/main/Board.java
+++ src/main/java/org/space_invaders/main/Board.java
@@ -417,7 +417,7 @@ public class Board extends JPanel {
 
             if (!bomb.isDestroyed()) {
 
-                bomb.setY(bomb.getY() - Commons.BOMB_SPEED);
+                bomb.setY(bomb.getY() + Commons.BOMB_SPEED);
 
                 if (bomb.getY() >= Commons.GROUND - Commons.BOMB_HEIGHT) {
 
